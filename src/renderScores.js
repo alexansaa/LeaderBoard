7fd0f788a46dd71b61e591ec8f2c0eac3175993c
alexansaa <alexan.saa@gmail.com>
2023-08-03 10:26:00 -0500

    Linters fix 3

--- src/renderScores.js
+++ src/renderScores.js
@@ -3,19 +3,18 @@ import _ from 'lodash';
 export const scoreCtn = document.querySelector('#scoresTable');
 
 export const renderFunction = (JsonObjectArray) => {
-	scoreCtn.innerHTML = '';
-	if (JsonObjectArray === null || JsonObjectArray.length === 0) {
-		return;
-	}
-	JsonObjectArray.forEach((element) => {
-		const scrElmnt = document.createElement('div');
-		scrElmnt.classList.add('element');
+  scoreCtn.innerHTML = '';
+  if (JsonObjectArray === null || JsonObjectArray.length === 0) {
+    return;
+  }
+  JsonObjectArray.forEach((element) => {
+    const scrElmnt = document.createElement('div');
+    scrElmnt.classList.add('element');
 
-		const content = document.createElement('p');
-		content.textContent = element.name + ': ' + element.score;
+    const content = document.createElement('p');
+    content.textContent = element.name + ': ' + element.score;
 
-		scrElmnt.appendChild(content);
-
-		scoreCtn.appendChild(scrElmnt);
-	});
+    scrElmnt.appendChild(content);
+    scoreCtn.appendChild(scrElmnt);
+  });
 };
